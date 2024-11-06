5a31317f4886d74a75cfba761b6603c6c1999a19
Gabriel Chamorro <gabriel.n.chamorro@gmail.com>
2024-11-05 22:57:11 -0300

    añadido pagina de catalogo junto a su servicio, y ts, fata realizar infinite scroll y guard

--- src/app/gb-ch-catalogo/gb-ch-catalogo.page.ts
+++ src/app/gb-ch-catalogo/gb-ch-catalogo.page.ts
@@ -1,18 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-
+//importo las interfazes
+import { RespuestaAPIProductosFormato } from '../interfaces/GB_CH_interfazRespuestaAPIProductos';
 //importaciones importantes para trabajar con formularios y validar estos (lo voy a hacer en el login y despues lo copio y pego al catalogo)
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
+//aqui pondre el ionview will enter para cargar los productos al ingresar a la pagina y el leave por si las moscas
+import { ViewWillEnter, ViewWillLeave } from '@ionic/angular';
+//importo subcripcion para poder manejar las subcripciones asignarle valores, para poder desuscribirme o suscribirme
+import { Subscription } from 'rxjs';
+import { GbChApiProductosService } from '../services/gb-ch-api-productos.service';
 @Component({
   selector: 'app-gb-ch-catalogo',
   templateUrl: './gb-ch-catalogo.page.html',
   styleUrls: ['./gb-ch-catalogo.page.scss'],
 })
-export class GBCHCatalogoPage implements OnInit {
+export class GBCHCatalogoPage implements ViewWillEnter, ViewWillLeave, OnInit {
+  private suscripcionProducto!: Subscription;
+  //puede que esto cause problemas
+  public datosProducto: RespuestaAPIProductosFormato = {products:[],total:0,skip:0,limit:0};
 
-  constructor() { }
+
+  constructor(
+    public producto: GbChApiProductosService,
+  ) { 
+  }
 
   ngOnInit() {
+  }
+
+  ionViewWillEnter(): void {
+    this.suscripcionProducto = this.producto.productos.subscribe(losDatos => {
+      if(losDatos){
+        this.datosProducto = losDatos;
+        console.log("datosProducto:",this.datosProducto)
+        console.log("losDatos",losDatos)
+        
+      }
+    }); 
+    this.producto.traerProductos()
+    
+  }
+
+  ionViewWillLeave(): void {
+    this.suscripcionProducto.unsubscribe();
   }
 
 }
--- src/app/services/gb-ch-api-productos.service.ts
+++ src/app/services/gb-ch-api-productos.service.ts
@@ -9,18 +9,38 @@ import { HttpClient } from '@angular/common/http';
 import { ProductosFormato } from '../interfaces/GB_CH_interfazProductos';
 import { RespuestaAPIProductosFormato } from '../interfaces/GB_CH_interfazRespuestaAPIProductos';
 
+import { InfiniteScrollCustomEvent } from '@ionic/angular';
 @Injectable({
   providedIn: 'root'
 })
 export class GbChApiProductosService {
   //en teoria deberia podria poder traer la variavel "usuarioActual" de el otro servicio
+  private $productos = new BehaviorSubject<RespuestaAPIProductosFormato | null>(null);
+  public productos = this.$productos.asObservable();
+
+  private $cargando = new BehaviorSubject<boolean>(false);
+  public cargando = this.$cargando.asObservable();
+
   public productoActual: ProductosFormato | null = null;
   public respuestaApiActual: RespuestaAPIProductosFormato | null = null;
-  private readonly UrlParaProductos = "https://dummyjson.com/auth/products"
+  private readonly UrlParaProductos = "https://dummyjson.com/products"
+  public salt: number = 0;
 
   constructor(
     private Http : HttpClient
   ) { }
+
+  traerProductos(){
+    this.$cargando.next(true)
+    this.Http.get<RespuestaAPIProductosFormato>
+    (this.UrlParaProductos)
+    .subscribe(datos =>{
+      this.$cargando.next(false);
+      this.$productos.next(datos);
+      console.log(datos)
+    })
+  }
+  
 
   
 }
